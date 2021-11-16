2c3c7fe778330015c91f5a5ffa3c7e631b29c740
clement0001 <clementtony111@yahoo.com>
2021-11-16 17:30:55 +0100

    updating our app with collection preview compontent

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-    <p>How many Hats do you want</p>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 
  function App() {
@@ -18,21 +11,12 @@ const HatsPage = () => (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
 }
 
-
-// function HatsPage() {
-//   return(
-//     <div>
-//       <h1>Hats Page</h1>
-//       <p>How many Hats do your want</p>
-//     </div>
-//   )
-// }
 
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,33 +14,33 @@ class Directory extends React.Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'hats'
+                    linkUrl: 'shop'
                   },
                   {
                     title: 'jackets',
                     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
-                    linkUrl: ''
+                    linkUrl: 'shop'
                   },
                   {
                     title: 'sneakers',
                     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
-                    linkUrl: ''
+                    linkUrl: 'shop'
                   },
                   {
                     title: 'womens',
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    linkUrl: ''
+                    linkUrl: 'shop'
                   },
                   {
                     title: 'mens',
                     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
-                    linkUrl: ''
+                    linkUrl: 'shop'
                   }
             ]
         }
