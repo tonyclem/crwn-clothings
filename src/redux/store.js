9a772b2b108380954760a722006526aa88241460
clement0001 <clementtony111@yahoo.com>
2021-11-24 21:52:36 +0100

    added styled-components, coverted homepahe, header and ect...

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-// hidden the developer tools
+// hidden the developer tools for every browers
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
