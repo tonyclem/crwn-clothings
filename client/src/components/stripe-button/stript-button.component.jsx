9c090975d5247673e776c78d2cda506d1665c972
clement0001 <clementtony111@yahoo.com>
2021-12-09 11:31:05 +0100

    separating our app into client folder, added express server to help handle backend stripe payment

---
+++ client/src/components/stripe-button/stript-button.component.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Jz33CL24QvDACf2ydntPpvSTsRdwuAKspXE2AfpUgZXXPynPrHP3NvTk6kJY5tlzJTP3DOXv3X7DDf97CV2kWdl00TOe5q2Pe';
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(_response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log('Payment error:', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. please make sure you use the provided credit cart'
+            );
+        });
+    };
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is €${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
